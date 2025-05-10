6686e7135efc9e36e04a4e10c4686c2e8279fd3e
Artur Ribeiro <88896435+arturribeiro04@users.noreply.github.com>
2025-05-10 15:41:40 -0300

    Capítulo 6/7
    - Final dos exercícios do capítulo 6 e início do capítulo 7

--- Capitulo_06/Exercicio_19.py
+++ Capitulo_06/Exercicio_19.py
@@ -1,6 +1,21 @@
-CONST_TEMP = (10,)
-dias = 5
+# Duas listas de exemplo
+lista1 = [1, 2, 3, 4, 5, 6]
+lista2 = [4, 5, 6, 7, 8, 9]
 
-total = dias * CONST_TEMP[0]
+conjunto1 = set(lista1)
+conjunto2 = set(lista2)
 
-print(total)
+comuns = conjunto1 & conjunto2
+print("Valores comuns às duas listas:", list(comuns))
+
+so_na_primeira = conjunto1 - conjunto2
+print("Valores que só existem na primeira lista:", list(so_na_primeira))
+
+so_na_segunda = conjunto2 - conjunto1
+print("Valores que só existem na segunda lista:", list(so_na_segunda))
+
+nao_repetidos = conjunto1 ^ conjunto2
+print("Elementos não repetidos nas duas listas:", list(nao_repetidos))
+
+lista1_sem_repetidos = list(conjunto1 - conjunto2)
+print("Primeira lista sem os elementos repetidos na segunda:", lista1_sem_repetidos)
